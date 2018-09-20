43134623e0ffde3d8cc3619770450573518fe806
TrashBadger <35270197+TrashBadger@users.noreply.github.com>
2018-09-19 20:05:41 -0500

    Update uniV5.js

--- Unicode V5/uniV5.js
+++ Unicode V5/uniV5.js
@@ -1,150 +1,94 @@
-var UniRanges=[
-["f00","f0b","f0d","f17","f1a","f34","f3a","f3d","fc7","fcc","fcf","fda"],
-["1400","167f"],
-["16a0","16f8"],
-["1681","169c"],
-["1720","1731"],
-["1740","1751"],
-["1760","176c"],
-["18b0","18f5"],
-["1900","191e"],
-["1a00","1a16"],
-["1b83","1ba0"],
-["1bc0","1be5"],
-["1c50","1c7f"],
-["1c80","1c88"],
-["1cc0","1cc7"],
-["1e00","1eff"],
-["20a0","20bf"],
-["2100","214f"],
-["2190","21ff"],
-["2200","22ff"],
-["2300","23ff"],
-["2440","244a"],
-["2460","2473"],
-["2500","257f"],
-["25a0","25ff"],
-["2600","26ff"],
-["2700","27bf"],
-["27c0","27ef"],
-["27f0","27ff"],
-["2900","297f"],
-["2980","29ff"],
-["2a00","2aff"],
-["2b00","2b73","2b76","2b95","2b98","2bb9","2bbd","2bc8","2bca","2bd0","2bec","2bef"],
-["2c00","2c2e"],
-["2c60","2c7f"],
-["2c80","2cee"],
-["2d00","2d25"],
-["2d30","2d67"],
-["2e00","2e39"],
-["3001","3029"],
-["31f0","31ff"],
-["3248","325f"],
-["4dc0","4dff"],
-["a000","a0ef"],
-["4dc0","4dff"],
-["a490","a4c6"],
-["a500","a59f"],
-["a640","a66e"],
-["a6a0","a6ef"],
-["a720","a7b9"],
-["a830","a839"],
-["a840","a877"],
-["a900","a925"],
-["a930","a946"],
-["aa80","aaaf"],
-["ab30","ab65"],
-["ab70","abbf"],
-["abc0","abe2"],
-["fe30","fe4f"],
-["10080","100fa"],
-["10107","10133"],
-["10140","1018e"],
-["10190","101a0"],
-["101d0","101fc"],
-["10280","1029c"],
-["102a0","102d0"],
-["10300","1034a"],
-["10350","10375"],
-["10380","1039D"],
-["103A0","103C3"],
-["10400","1044F"],
-["104B0","104D3","104D8","104FB"],
-["10500","10527"],
-["10530","10563"],
-["10600","106BF"],
-["10860","1087F"],
-["108E0","108F2"],
-["10900","1091B"],
-["10980","1099F"],
-["10A60","10A7F"],
-["10A80","10A9F"],
-["10C00","10C48"],
-["10C80","10CB2","10CC0","10CF2"],
-["11005","11037"],
-["11580","115AE","115C1","115DB"],
-["11660","1166C"],
-["118A0","118F2"],
-["11A0B","11A32"],
-["11A5C","11A83","11A9A","11AA2"],
-["11C70","11C8F"],
-["12000","12399"],
-["12400","12474"],
-["12480","12543"],
-["13000","130DF"],
-["14400","14646"],
-["16800","16A38"],
-["16F00","16F44"],
-["1BC00","1BC6A"],
-["1D100","1D126","1D129","1D158","1D15A","1D164"],
-["1D200","1D241"],
-["1D300","1D356"],
-["1D360","1D378"],
-["1D800","1D9F0"],
-["1E800","1E8C4"],
-["1F000","1F02B"],
-["1F030","1F093"],
-["1F300","1F5FF"],
-["1F600","1F64F"],
-["1F650","1F67F"],
-["1F680","1F6D4"],
-["1F700","1F773"],
-["1F780","1F7D8"],
-["1F800","1F80B","1F860","1F887"],
-["1F900","1F90B","1F910","1F93E","1F940","1F970","1F97C","1F9A"]
-];
-var UniBlocks=[];
-var UniPairs=[];
+var uniArray=[];
+var uniPairs=[];
 var asciiArray=[];
 var asciiPairs=[];
-for( var i = 32; i <= 126; i++ ){
-    asciiArray[i-32] = String.fromCharCode( i );
+for( var i = 32; i <= 127; i++ ){
+    asciiArray[i-32] = String.fromCharCode(i);
+};
+for( var i = 256; i <= 687; i++ ){
+    uniArray[i-256] = String.fromCharCode(i);
 };
 asciiSize=asciiArray.length;
+uniSize=uniArray.length;
 //HTML
-msgOutput=document.getElementById('outputMsg')
+msgOutput=document.getElementById('outputMsg');
+inputMsg=document.getElementById('inputMsg');
+inputPass=document.getElementById('inputPass');
 //FUNCTIONS
-function genBlocks(){
-	for(var i=0;i<UniRanges.length;i++){
-	for(var ii=0;ii<UniRanges[i].length;ii++){
-		UniBlocks[i+ii*(UniRanges[i].length-1)]=parseInt(UniRanges[i][ii], 16);
-	};};
-};
-function genAsciiPairs(){
-	for(var i=0;i<asciiSize;i++){
-	for(var ii=0;ii<asciiSize;ii++){
-		asciiPairs[ii+i*(asciiSize-1)]=asciiArray[i]+asciiArray[ii];
-	};};
-};
-function genUniPairs(){
-	for(i=0;i<UniBlocks.length;i+=2){
-	for(ii=0;ii<=UniBlocks[i+1]-UniBlocks[i];ii++){
-		UniPairs[i*(UniBlocks.length-1)+ii]=UniBlocks[i]+ii;
-	};};
-};
-function testUnicodeList(){
-};
-genBlocks();
-genAsciiPairs();
-genUniPairs();
+function genASCIIpairs(){
+	count=0;
+	for(i=0;i<asciiSize;i++){
+	for(ii=0;ii<asciiSize;ii++){
+		asciiPairs[count]=asciiArray[i]+asciiArray[ii];
+		count++;
+	}}
+}
+genASCIIpairs();
+function genUNIpairs(){
+	count=0;
+	for(i=0;i<uniSize;i++){
+	for(ii=0;ii<uniSize;ii++){
+		uniPairs[count]=uniArray[i]+uniArray[ii];
+		count++;
+	}}
+}
+genUNIpairs();
+//GEN END
+function normalEncryption(cryptoMode){
+	if(inputMsg.value.length<1){return 2;}
+	if(inputPass.value==undefined||inputPass.value==""){
+		inputPass.value=asciiArray[0];
+	}
+	printString="[SRT]";
+	inputMsgArray=inputMsg.value.split("");
+	inputPassArray=inputPass.value.split("");
+	inputPassSize=inputPassArray.length;
+	for(i=0;i<inputMsg.value.length;i++){
+		indexMsgA = asciiArray.indexOf(inputMsgArray[i]);
+		indexPassA = asciiArray.indexOf(inputPassArray[i%inputPassSize]);
+		indexOutA = (indexMsgA+cryptoMode*indexPassA+asciiSize)%asciiSize;
+		//console.log("[i]:"+i,"Index:"+indexOutA,"partA:"+indexMsgA,"partB:"+indexPassA);
+		printString=printString+asciiArray[indexOutA];
+	}
+	printString=printString.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+	msgOutput.innerHTML=printString+"[END]";
+	//console.log("output:"+printString);
+}
+//Uni
+function uniEncrypt(cryptoMode){
+	if(inputMsg.value.length<1){return 2;}
+	if(inputMsg.value.length%2>0){
+		inputMsg.value+=" ";
+	}
+	printString="[SRT]";
+	inputMsgArray=inputMsg.value.split("");
+	if(cryptoMode>0){
+		if(inputMsgArray[0].charCodeAt(0)>127){return 2;}		
+		for(i=0;i<inputMsgArray.length;i+=2){
+			searchPair = inputMsgArray[i]+inputMsgArray[i+1];
+			indexMsgA = asciiPairs.indexOf(searchPair);
+			printString = printString+uniPairs[indexMsgA];
+			//console.log(searchPair,indexMsgA);
+		}
+	}else{
+		if(inputMsgArray[0].charCodeAt(0)<127){return 2;}
+		for(i=0;i<inputMsgArray.length;i+=2){
+			searchPair = inputMsgArray[i]+inputMsgArray[i+1];
+			indexMsgA = uniPairs.indexOf(searchPair);
+			printString = printString+asciiPairs[indexMsgA];
+			//console.log(searchPair,indexMsgA);
+		}
+	}
+	printString=printString.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+	msgOutput.innerHTML=printString+"[END]";
+}
+//test functions
+function printArray(myArray){
+	printString="";
+	for(i=0;i<myArray.length;i++){
+		printString=printString+myArray[i]+"_";
+	}
+	printString=printString.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+	msgOutput.innerHTML=printString;
+}
+//myOutput=myOutput.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
